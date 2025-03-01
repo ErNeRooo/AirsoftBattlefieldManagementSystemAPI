c8f1940eea0f350cdcc13900f766b052d226d7f3
ErNeRooo <erneroooooo@gmail.com>
2025-03-01 13:43:19 +0100

    add RoomController endpoints

---
+++ Controllers/RoomController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AirsoftBattlefieldManagementSystemAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RoomController : ControllerBase
+    {
+        private readonly ILogger<RoomController> _logger;
+
+        public RoomController(ILogger<RoomController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("id/{id}")]
+        public ActionResult<string> GetRoom(int id)
+        {
+            return Ok($"Room {id}");
+        }
+
+        [HttpPost("id/{id}")]
+        public ActionResult<string> PostRoom(int id)
+        {
+            return Created();
+        }
+
+        [HttpPut("id/{id}")]
+        public ActionResult<string> PutRoom(int id)
+        {
+            return Ok($"put {id}");
+        }
+
+        [HttpDelete("id/{id}")]
+        public ActionResult<string> DeleteRoom(int id)
+        {
+            return NoContent();
+        }
+    }
+}
