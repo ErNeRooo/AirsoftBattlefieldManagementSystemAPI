be3b8144f412a9cee22a09d28a02f40ab26bb931
ErNeRooo <erneroooooo@gmail.com>
2025-03-02 12:15:20 +0100

    add migration RenameColumnIdToRoomIdOfRoomTable

---
+++ Migrations/20250302111416_RenameColumnIdToRoomIdOfRoomTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AirsoftBattlefieldManagementSystemAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameColumnIdToRoomIdOfRoomTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Room",
+                newName: "RoomId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RoomId",
+                table: "Room",
+                newName: "Id");
+        }
+    }
+}
