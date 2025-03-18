2aadecfd5b2b207304f3f7f7ccede0f55ccb0719
ErNeRooo <erneroooooo@gmail.com>
2025-03-18 19:38:37 +0100

    change kill request validation

---
+++ src/Models/Validators/Create/CreateKillDtoValidator.cs
@@ -0,0 +1,20 @@
+﻿using AirsoftBattlefieldManagementSystemAPI.Models.Dtos.Create;
+using AirsoftBattlefieldManagementSystemAPI.Models.Entities;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using System.Text.RegularExpressions;
+
+namespace AirsoftBattlefieldManagementSystemAPI.Models.Validators
+{
+    public class CreateKillDtoValidator : AbstractValidator<CreateKillDto>
+    {
+        public CreateKillDtoValidator()
+        {
+            RuleFor(l => l.Longitude).NotEmpty();
+            RuleFor(l => l.Latitude).NotEmpty();
+            RuleFor(l => l.Accuracy).NotEmpty();
+            RuleFor(l => l.Bearing).NotEmpty();
+            RuleFor(l => l.Time).NotEmpty();
+        }
+    }
+}
