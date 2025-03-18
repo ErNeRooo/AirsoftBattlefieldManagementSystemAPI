a4bdfd70e7166d782f16ccae68abd36cbe99f9af
ErNeRooo <erneroooooo@gmail.com>
2025-03-18 18:35:01 +0100

    change battle request validation

--- src/Models/Dtos/Create/CreateBattleDto.cs
+++ src/Models/Dtos/Create/CreateBattleDto.cs
@@ -4,10 +4,8 @@ namespace AirsoftBattlefieldManagementSystemAPI.Models.Dtos.Create
 {
     public class CreateBattleDto
     {
-        [Required]
         public string Name { get; set; }
         public bool IsActive { get; set; } = false;
-        [Required]
         public int RoomId { get; set; }
     }
 }
--- src/Models/Dtos/Update/UpdateBattleDto.cs
+++ src/Models/Dtos/Update/UpdateBattleDto.cs
@@ -4,9 +4,7 @@ namespace AirsoftBattlefieldManagementSystemAPI.Models.Dtos.Update
 {
     public class UpdateBattleDto
     {
-        [Required]
         public string Name { get; set; }
-        [Required]
         public bool IsActive { get; set; }
     }
 }
