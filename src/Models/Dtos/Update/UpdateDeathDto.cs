f8070fbeb6042c848729a9797560285a367b8643
ErNeRooo <erneroooooo@gmail.com>
2025-03-18 19:41:30 +0100

    change death request validation

--- src/Models/Dtos/Update/UpdateDeathDto.cs
+++ src/Models/Dtos/Update/UpdateDeathDto.cs
@@ -4,15 +4,10 @@ namespace AirsoftBattlefieldManagementSystemAPI.Models.Dtos.Update
 {
     public class UpdateDeathDto
     {
-        [Required]
         public decimal Longitude { get; set; }
-        [Required]
         public decimal Latitude { get; set; }
-        [Required]
         public short Accuracy { get; set; }
-        [Required]
         public short Bearing { get; set; }
-        [Required]
         public DateTime Time { get; set; }
     }
 }
