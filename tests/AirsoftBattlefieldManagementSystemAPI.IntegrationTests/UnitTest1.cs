47f223fa18d4ac4e0b50854ea22daca73e831000
ErNeRooo <erneroooooo@gmail.com>
2025-04-02 16:32:31 +0200

    init Integration Tests Project

---
+++ tests/AirsoftBattlefieldManagementSystemAPI.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace AirsoftBattlefieldManagementSystemAPI.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
