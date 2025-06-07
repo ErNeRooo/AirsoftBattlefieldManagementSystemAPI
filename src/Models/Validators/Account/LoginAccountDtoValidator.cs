31713b782172a90f4650164967cdff9783282679
ErNeRooo <erneroooooo@gmail.com>
2025-06-07 22:50:54 +0200

    add LogInAccountDtoValidatorTests

--- src/Models/Validators/Account/LoginAccountDtoValidator.cs
+++ src/Models/Validators/Account/LoginAccountDtoValidator.cs
@@ -1,4 +1,5 @@
-﻿using AirsoftBattlefieldManagementSystemAPI.Models.BattleManagementSystemDbContext;
+﻿using System.Text.RegularExpressions;
+using AirsoftBattlefieldManagementSystemAPI.Models.BattleManagementSystemDbContext;
 using AirsoftBattlefieldManagementSystemAPI.Models.Dtos.Account;
 using FluentValidation;
 
@@ -14,7 +15,12 @@ namespace AirsoftBattlefieldManagementSystemAPI.Models.Validators.Account
 
                 if(isNotFound) context.AddFailure($"Account with email {value} not found");
             });
-            RuleFor(a => a.Password).NotEmpty().MinimumLength(10);
+            RuleFor(a => a.Password).NotEmpty().MinimumLength(10).Custom((value, context) =>
+            {
+                bool matchRegex = Regex.IsMatch(value, "^(?=.*[A-Z])(?=.*\\d)(?=.*[^a-zA-Z0-9]).+$");
+                
+                if(!matchRegex) context.AddFailure("Password must have at least 1 special character, 1 digit and 1 upper case letter");
+            });
         }
     }
 }
