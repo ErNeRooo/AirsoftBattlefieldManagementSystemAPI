529366128936fe7805a408cda0d1b2c97b850635
ErNeRooo <erneroooooo@gmail.com>
2025-06-11 20:57:09 +0200

    add BattleController.Delete tests

---
+++ tests/AirsoftBattlefieldManagementSystemAPI.Tests/Controllers/BattleController/BattleControllerDeleteTests.cs
@@ -0,0 +1,68 @@
+using System.Net;
+using Shouldly;
+
+namespace AirsoftBattlefieldManagementSystemAPI.Tests.Controllers.BattleController;
+
+public class BattleControllerDeleteTests
+{
+    private HttpClient _client;
+    private string _endpoint = "battle/id/";
+    
+    public BattleControllerDeleteTests()
+    {
+        CustomWebApplicationFactory<Program> factory = new CustomWebApplicationFactory<Program>();
+        _client = factory.CreateClient();
+    }
+    
+    [Theory]
+    [InlineData(1, 1)]
+    [InlineData(3, 2)]
+    public async void Delete_ValidId_ReturnsNoContent(int senderPlayerId, int battleId)
+    {
+        CustomWebApplicationFactory<Program> factory = new CustomWebApplicationFactory<Program>(senderPlayerId);
+        _client = factory.CreateClient();
+        
+        var response = await _client.DeleteAsync($"{_endpoint}{battleId}");
+        
+        response.StatusCode.ShouldBe(HttpStatusCode.NoContent);
+    }
+    
+    [Theory]
+    [InlineData("1.0")]
+    [InlineData("e")]
+    public async void Delete_NotValidId_ReturnsBadRequest(string battleId)
+    {
+        var response = await _client.DeleteAsync($"{_endpoint}{battleId}");
+        
+        response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
+    }
+    
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    [InlineData(112354)]
+    public async void Delete_NotExistingBattle_ReturnsNotFound(int battleId)
+    {
+        var response = await _client.DeleteAsync($"{_endpoint}{battleId}");
+        
+        response.StatusCode.ShouldBe(HttpStatusCode.NotFound);
+    }
+    
+    [Theory]
+    [InlineData(2, 1)]
+    [InlineData(2, 2)]
+    [InlineData(4, 2)]
+    [InlineData(5, 1)]
+    [InlineData(5, 2)]
+    [InlineData(6, 2)]
+    [InlineData(6, 1)]
+    public async void Delete_AsNotRoomAdmin_ReturnsForbidden(int senderPlayerId, int battleId)
+    {
+        CustomWebApplicationFactory<Program> factory = new CustomWebApplicationFactory<Program>(senderPlayerId);
+        _client = factory.CreateClient();
+        
+        var response = await _client.DeleteAsync($"{_endpoint}{battleId}");
+        
+        response.StatusCode.ShouldBe(HttpStatusCode.Forbidden);
+    }
+}
