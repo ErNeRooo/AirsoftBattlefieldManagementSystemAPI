554fe31b9be0523307eb45ecb1e5a852abae8015
ErNeRooo <erneroooooo@gmail.com>
2025-06-12 15:24:46 +0200

    add KillController.Delete tests

---
+++ tests/AirsoftBattlefieldManagementSystemAPI.Tests/Controllers/KillController/KillControllerDeleteTests.cs
@@ -0,0 +1,65 @@
+using System.Net;
+using Shouldly;
+
+namespace AirsoftBattlefieldManagementSystemAPI.Tests.Controllers.KillController;
+
+public class KillControllerDeleteTests
+{
+    private HttpClient _client;
+    private string _endpoint = "kill/id/";
+
+    public KillControllerDeleteTests()
+    {
+        
+        CustomWebApplicationFactory<Program> factory = new CustomWebApplicationFactory<Program>();
+        _client = factory.CreateClient();
+    }
+    
+        
+    [Theory]
+    [InlineData(1, 1)]
+    [InlineData(1, 6)]
+    [InlineData(2, 3)]
+    [InlineData(3, 5)]
+    [InlineData(4, 7)]
+    public async void Delete_ValidId_ReturnsNoContent(int senderPlayerId, int killId)
+    {
+        CustomWebApplicationFactory<Program> factory = new CustomWebApplicationFactory<Program>(senderPlayerId);
+        _client = factory.CreateClient();
+        
+        var response = await _client.DeleteAsync($"{_endpoint}{killId}");
+        
+        response.StatusCode.ShouldBe(HttpStatusCode.NoContent);
+    }
+    
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    [InlineData(112354)]
+    public async void Delete_NotExistingKill_ReturnsNotFound(int killId)
+    {
+        var response = await _client.DeleteAsync($"{_endpoint}{killId}");
+        
+        response.StatusCode.ShouldBe(HttpStatusCode.NotFound);
+    }
+    
+    [Theory]
+    [InlineData(1, 3)]
+    [InlineData(2, 1)]
+    [InlineData(3, 7)]
+    [InlineData(4, 5)]
+    [InlineData(4, 1)]
+    [InlineData(1, 7)]
+    [InlineData(6, 4)]
+    [InlineData(5, 4)]
+    [InlineData(5, 7)]
+    public async void Delete_AsOtherPlayer_ReturnsForbidden(int senderPlayerId, int killId)
+    {
+        CustomWebApplicationFactory<Program> factory = new CustomWebApplicationFactory<Program>(senderPlayerId);
+        _client = factory.CreateClient();
+        
+        var response = await _client.DeleteAsync($"{_endpoint}{killId}");
+        
+        response.StatusCode.ShouldBe(HttpStatusCode.Forbidden);
+    }
+}
